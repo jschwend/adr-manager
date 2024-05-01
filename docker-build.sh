939e3ded83e469560fb648668288929c5b9570a5
Jack Schwenderman <jack_schwenderman@mckinsey.com>
2024-05-01 17:32:40 -0400

    Added AZ update to script

--- docker-build.sh
+++ docker-build.sh
@@ -6,7 +6,14 @@ else
     version=$1
     podman build -t adr-manager . --arch linux/amd64 --build-arg DOCKER_TAG=$version
     podman image tag localhost/adr-manager mckinsey-sfcseate-adr-docker-local.jfrog.io/sfcseate-adr:$version
+    podman image push mckinsey-sfcseate-adr-docker-local.jfrog.io/sfcseate-adr:$version
+    az webapp config container set --name mckadr \
+                                  --resource-group WebApps \
+                                  --container-image-name "mckinsey-sfcseate-adr-docker.jfrog.io/sfcseate-adr:$version" \
+                                  --container-registry-url mckinsey-sfcseate-adr-docker.jfrog.io \
+                                  --container-registry-user ${ARTIFACTORY_USER} \
+                                  --container-registry-password ${ARTIFACTORY_PASSWORD} \
+                                  --slot dev
     podman image tag localhost/adr-manager mckinsey-sfcseate-adr-docker-local.jfrog.io/sfcseate-adr:latest
     podman image push mckinsey-sfcseate-adr-docker-local.jfrog.io/sfcseate-adr:latest
-    podman image push mckinsey-sfcseate-adr-docker-local.jfrog.io/sfcseate-adr:$version
 fi
